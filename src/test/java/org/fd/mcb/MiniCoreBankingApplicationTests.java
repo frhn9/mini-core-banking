b93b6a34b3a32b2087dd53484ae8dc50deb80bee
frhn9 <just_fadhyl@hotmail.co.id>
2025-09-03 11:08:04 +0700

    init

---
+++ src/test/java/org/fd/mcb/MiniCoreBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fd.mcb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCoreBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
